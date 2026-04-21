e63b0f5c8e04bb81db08e293f77881256a97904b
FIDELACHU <fidelonofre28@gmail.com>
2026-04-21 09:09:58 -0700

    Fix: prop icon_url en componente chuck-card

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 Vue.component('chuck-card', {
-    props: ['value'],
+    props: ['value', 'icon_url'],
     template: `
         <div class='card'>
-            <h3>{{ value }}</h3>
+        <img :src="icon_url" alt="Chuck Norris">
+            <h4 class='margen'>{{ value }}</h4>
         </div>  `
 });
 
@@ -10,11 +11,21 @@ var app = new Vue({
     el: '#app',
     data: {
         chuck: [
-        { "value": 'Chuck Norris can skydive into outer space.' },
-        { "value": 'The chief export of Chuck Norris is pain.' },
-        { "value": 'Chuck Norris doesnt read books. He stares them down until he gets the information he wants.' },
-        { "value": 'Time waits for no man. Unless that man is Chuck Norris.' },
-        { "value": 'If you spell Chuck Norris in Scrabble, you win. Forever.' }
+        { 
+            "icon_url": "https://img.europapress.es/fotoweb/fotonoticia_20150310130850-732359_1200.jpg",
+            "value": 'Chuck Norris can skydive into outer space.' },
+        { 
+            "icon_url": "https://img.europapress.es/fotoweb/fotonoticia_20150310130850-732359_1200.jpg",
+            "value": 'The chief export of Chuck Norris is pain.' },
+        { 
+            "icon_url": "https://img.europapress.es/fotoweb/fotonoticia_20150310130850-732359_1200.jpg",
+            "value": 'Chuck Norris doesnt read books. He stares them down until he gets the information he wants.' },
+        { 
+            "icon_url": "https://img.europapress.es/fotoweb/fotonoticia_20150310130850-732359_1200.jpg",
+            "value": 'Time waits for no man. Unless that man is Chuck Norris.' },
+        { 
+            "icon_url": "https://img.europapress.es/fotoweb/fotonoticia_20150310130850-732359_1200.jpg",
+            "value": 'If you spell Chuck Norris in Scrabble, you win. Forever.' }
         ]
     },
 });
